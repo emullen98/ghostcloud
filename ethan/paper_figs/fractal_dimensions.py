7c20ffec91dd5279cf5629e5b3526b6438c58ad3
emullen98 <emullen98@gmail.com>
2025-11-22 10:01:57 -0600

    Setting up cluster pva bootstrapping

---
+++ ethan/paper_figs/fractal_dimensions.py
@@ -0,0 +1,28 @@
+"""
+Created Nov 19 2025
+Updated Nov 19 2025
+
+Plots the perimeter vs. area scatter plot for the PNG files
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/Users/emullen98/Desktop')
+import helper_scripts as hs
+
+
+def plot_pva_allthresh():
+
+    return 'temp'
+
+
+def plot_pva_argmax():
+
+    return 'temp'
+
+
+
+if __name__ == "__main__":
+    plot_pva_allthresh()
+    plot_pva_argmax()
